11032842d3b92c39fc425657d9ef491487b09913
anhtu <anhtu293@gmail.com>
2024-10-31 17:24:03 +0100

    metrics with docker compose

--- ocr-app/app/main.py
+++ ocr-app/app/main.py
@@ -2,6 +2,12 @@ import easyocr
 import os
 from PIL import Image
 import imagehash
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import get_tracer_provider, set_tracer_provider
 from collections import OrderedDict
 from io import BytesIO
 import pika
@@ -10,6 +16,23 @@ from loguru import logger
 import base64
 import numpy as np
 from .utils import get_sentence
+
+
+METRIC_SERVICE_NAME = os.getenv("METRIC_SERVICE_NAME")
+METRIC_SERVICE_VERSION = os.getenv("METRIC_SERVICE_VERSION")
+
+# Setup Jaeger
+JAEGER_AGENT_HOST = os.getenv("JAEGER_AGENT_HOST")
+JAEGER_AGENT_PORT = int(os.getenv("JAEGER_AGENT_PORT"))
+resource = Resource(attributes={SERVICE_NAME: METRIC_SERVICE_NAME})
+set_tracer_provider(TracerProvider(resource=resource))
+tracer = get_tracer_provider().get_tracer(METRIC_SERVICE_NAME, METRIC_SERVICE_VERSION)
+jaeger_exporter = JaegerExporter(
+    agent_host_name=JAEGER_AGENT_HOST,
+    agent_port=JAEGER_AGENT_PORT,
+)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+get_tracer_provider().add_span_processor(span_processor)
 
 
 RABBITMQ_USER = os.getenv("RABBITMQ_USER")
@@ -39,40 +62,51 @@ logger.info("OCR model loaded")
 
 def process_ocr_task(ch, method, properties, body):
     try:
-        task = json.loads(body)
-        logger.info(f"Processing image: {task['task_id']}")
-        image = Image.open(BytesIO(base64.b64decode(task["data"].encode("utf-8"))))
-        image_hash = imagehash.average_hash(image)
+        with tracer.start_as_current_span("ocr-service") as span:
+            with tracer.start_as_current_span("load_data", links=[trace.Link(span.get_span_context())]):
+                task = json.loads(body)
+                logger.info(f"Processing image: {task['task_id']}")
+                image = Image.open(BytesIO(base64.b64decode(task["data"].encode("utf-8"))))
+                image_hash = imagehash.average_hash(image)
 
-        if image_hash in cache:
-            return cache[image_hash]
+            if image_hash in cache:
+                channel.basic_publish(
+                    exchange="",
+                    routing_key="ocr_results",
+                    body=json.dumps({"task_id": task["task_id"], "result": cache[image_hash]}),
+                )
+                channel.basic_ack(delivery_tag=method.delivery_tag)
+                return
 
-        detection = reader.readtext(image)
+            with tracer.start_as_current_span("ocr_detection", links=[trace.Link(span.get_span_context())]):
+                detection = reader.readtext(image)
 
-        # Get median height of bboxes
-        bboxes_heights = []
-        for bbox, text, prob in detection:
-            (top_left, _, bottom_right, _) = bbox
-            bboxes_heights.append(bottom_right[1] - top_left[1])
-        bbox_height = int(np.median(bboxes_heights))
+            # Get median height of bboxes
+            with tracer.start_as_current_span("ocr_detection_height", links=[trace.Link(span.get_span_context())]):
+                bboxes_heights = []
+                for bbox, text, prob in detection:
+                    (top_left, _, bottom_right, _) = bbox
+                    bboxes_heights.append(bottom_right[1] - top_left[1])
+                bbox_height = int(np.median(bboxes_heights))
 
-        # Create the final result
-        result = get_sentence(detection)
+            # Create the final result
+            with tracer.start_as_current_span("ocr_merge_sentence", links=[trace.Link(span.get_span_context())]):
+                result = get_sentence(detection)
 
-        bboxes = [box[0] for box in result]
-        texts = [box[1] for box in result]
-        result = {"bboxes": bboxes, "texts": texts, "bbox_height": bbox_height}
+            bboxes = [box[0] for box in result]
+            texts = [box[1] for box in result]
+            result = {"bboxes": bboxes, "texts": texts, "bbox_height": bbox_height}
 
-        if len(cache) >= CACHE_SIZE:
-            cache.popitem(last=False)
-        cache[image_hash] = result
+            if len(cache) >= CACHE_SIZE:
+                cache.popitem(last=False)
+            cache[image_hash] = result
 
-        channel.basic_publish(
-            exchange="",
-            routing_key="ocr_results",
-            body=json.dumps({"task_id": task["task_id"], "result": result}),
-        )
-        channel.basic_ack(delivery_tag=method.delivery_tag)
+            channel.basic_publish(
+                exchange="",
+                routing_key="ocr_results",
+                body=json.dumps({"task_id": task["task_id"], "result": result}),
+            )
+            channel.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         logger.error(f"Error processing OCR task: {e}")
         channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
--- trans-app/app/main.py
+++ trans-app/app/main.py
@@ -3,6 +3,29 @@ import pika
 import json
 from loguru import logger
 import os
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import get_tracer_provider, set_tracer_provider
+
+
+METRIC_SERVICE_NAME = os.getenv("METRIC_SERVICE_NAME")
+METRIC_SERVICE_VERSION = os.getenv("METRIC_SERVICE_VERSION")
+
+# Setup Jaeger
+JAEGER_AGENT_HOST = os.getenv("JAEGER_AGENT_HOST")
+JAEGER_AGENT_PORT = int(os.getenv("JAEGER_AGENT_PORT"))
+resource = Resource(attributes={SERVICE_NAME: METRIC_SERVICE_NAME})
+set_tracer_provider(TracerProvider(resource=resource))
+tracer = get_tracer_provider().get_tracer(METRIC_SERVICE_NAME, METRIC_SERVICE_VERSION)
+jaeger_exporter = JaegerExporter(
+    agent_host_name=JAEGER_AGENT_HOST,
+    agent_port=JAEGER_AGENT_PORT,
+)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+get_tracer_provider().add_span_processor(span_processor)
 
 
 RABBITMQ_USER = os.getenv("RABBITMQ_USER")
@@ -24,24 +47,29 @@ logger.info("Translation model loaded")
 
 def process_translation_task(ch, method, properties, body):
     try:
-        task = json.loads(body)
-        logger.info(f"Processing translation task: {task['task_id']}")
+        with tracer.start_as_current_span("translation-service") as span:
+            with tracer.start_as_current_span("load_data", links=[trace.Link(span.get_span_context())]):
+                task = json.loads(body)
+                logger.info(f"Processing translation task: {task['task_id']}")
 
-        translations = []
-        for i, input in enumerate(task["texts"]):
-            eng_input = f"en: {input}"
-            inputs = tokenizer(eng_input, return_tensors="pt", padding=True).input_ids
-            outputs = model.generate(inputs, max_length=512)
-            result = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-            result = result.replace("vi: ", "")
-            translations.append(result)
+            translations = []
 
-        channel.basic_publish(
-            exchange="",
-            routing_key="translation_results",
-            body=json.dumps({"task_id": task["task_id"], "result": translations}),
-        )
-        channel.basic_ack(delivery_tag=method.delivery_tag)
+            with tracer.start_as_current_span("translation-service-translate", links=[trace.Link(span.get_span_context())]):
+                for i, input in enumerate(task["texts"]):
+                    eng_input = f"en: {input}"
+                    inputs = tokenizer(eng_input, return_tensors="pt", padding=True).input_ids
+                    outputs = model.generate(inputs, max_length=512)
+                    result = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+                    result = result.replace("vi: ", "")
+                    translations.append(result)
+
+            with tracer.start_as_current_span("translation-service-publish", links=[trace.Link(span.get_span_context())]):
+                channel.basic_publish(
+                    exchange="",
+                    routing_key="translation_results",
+                    body=json.dumps({"task_id": task["task_id"], "result": translations}),
+                )
+                channel.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         logger.error(f"Error processing translation task: {e}")
         channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
